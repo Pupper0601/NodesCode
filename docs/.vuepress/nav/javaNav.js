b0b6501b98b2f14e7f5cf51b87a23d070cf4f290
Puppper <58965954+Pupper0601@users.noreply.github.com>
2022-07-30 12:01:04 +0800

    导航栏调整

--- docs/.vuepress/nav/javaNav.js
+++ docs/.vuepress/nav/javaNav.js
@@ -1,7 +1,7 @@
 // Java导航栏
 module.exports = {
   text: 'Java', link: '/java/',  // 目录页链接，此处 link 是 vdoing 主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-  items: [
-    {text: 'Java基础', link: '/java/basis/'},
-  ]
+  // items: [
+  //   {text: 'Java基础', link: '/java/basis/'},
+  // ]
 }
--- docs/.vuepress/nav/linuxNav.js
+++ docs/.vuepress/nav/linuxNav.js
@@ -1,7 +1,7 @@
 // 服务器导航
 module.exports = {
     text: ' Linux', link: '/linux/',
-    items: [
-        { text: 'Linux 基础', link: '/linux/basis/'}
-    ]
+    // items: [
+    //     { text: 'Linux 基础', link: '/linux/basis/'}
+    // ]
 }
